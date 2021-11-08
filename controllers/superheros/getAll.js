d7d2ca5b25396de28194a6faf1b55270512d6262
Kyrylo Moskalenko <72569852+Kylory@users.noreply.github.com>
2021-11-08 19:42:00 +0200

    Update getAll.js

--- controllers/superheros/getAll.js
+++ controllers/superheros/getAll.js
@@ -2,7 +2,7 @@ const { getAllSuperheroes } = require('../../model/superheros/index')
 
 const getAll = async (req, res) => {
   const { page, limit } = req.query
-  const superheroes = await getAllSuperheroes(page, limit)
+  const superheros = await getAllSuperheroes(page, limit)
 
   res.status(200).json({ superheros })
 }
