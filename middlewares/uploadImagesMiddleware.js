b26b21ca2618ed517a6daa2c2370edbdd660ab19
Kyrylo Moskalenko <krllmsk@gmail.com>
2021-11-10 00:10:12 +0200

    images

---
+++ middlewares/uploadImagesMiddleware.js
@@ -0,0 +1,21 @@
+const multer = require('multer')
+const path = require('path')
+
+const tempDir = path.join(__dirname, '../', 'temp')
+const uploadConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  // limits: {
+  //   fileSize: 4096,
+  // },
+})
+
+const uploadImagesMiddleware = multer({
+  storage: uploadConfig,
+})
+
+module.exports = uploadImagesMiddleware
