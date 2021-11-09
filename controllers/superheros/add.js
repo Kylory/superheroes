b73291f699660dc8941823f6d0f757ed73a308b1
Kyrylo Moskalenko <72569852+Kylory@users.noreply.github.com>
2021-11-09 11:03:19 +0200

    Update add.js

--- controllers/superheros/add.js
+++ controllers/superheros/add.js
@@ -2,20 +2,20 @@ const { addSuperhero } = require('../../model/superheros')
 const { BadRequest } = require('http-errors')
 const Joi = require('joi')
 
-// const joiSchema = Joi.object({
-//   nickname: Joi.string(),
-//   real_name: Joi.string(),
-//   origin_description: Joi.string(),
-//   superpowers: Joi.string(),
-//   catch_phrase: Joi.string(),
-// })
+const joiSchema = Joi.object({
+  nickname: Joi.string(),require(),
+  real_name: Joi.string(),
+  origin_description: Joi.string(),
+  superpowers: Joi.string(),
+  catch_phrase: Joi.string(),
+})
 
 const add = async (req, res) => {
-//   const { error } = joiSchema.validate(req.body)
+  const { error } = joiSchema.validate(req.body)
 
-//   if (error) {
-//     throw new BadRequest('missing required field')
-//   }
+  if (error) {
+    throw new BadRequest('missing required field')
+  }
 
   const newSuperhero = await addSuperhero(req.body)
 
